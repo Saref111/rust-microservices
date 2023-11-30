196b4b5cddcbee07dbd4b8f429ee1148a93d05a3
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-11-30 14:37:33 +0200

    build is done

--- build.rs
+++ build.rs
@@ -1,7 +1,13 @@
 use std::env;
+use homedir;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    env::set_var("PROTOC", format!("C:\\Users\\Ivan_Borovyk\\.local\\bin\\protoc.exe"));
+    if env::var("PROTOC").is_err() {
+        let u_name = env::var("USERNAME").unwrap();
+        let home = homedir::get_home(u_name).unwrap().ok_or("Cannot find homedir").unwrap();
+        let home = home.to_str().unwrap();
+        env::set_var("PROTOC", format!("{home}\\.local\\bin\\protoc.exe"));
+    }
     tonic_build::compile_protos("proto/authentication.proto")?;
     Ok(())
 }
