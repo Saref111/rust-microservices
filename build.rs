b492a9cc9188e6d80a819e12b3862cc3e34c4f60
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-11-30 10:20:56 +0200

    installed PROTOC for windows

--- build.rs
+++ build.rs
@@ -1,4 +1,7 @@
+use std::env;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env::set_var("PROTOC", format!("C:\\Users\\Ivan_Borovyk\\.local\\bin\\protoc.exe"));
     tonic_build::compile_protos("proto/authentication.proto")?;
     Ok(())
 }
