fe181fb28cfc572fdca46ac0b44b4a333f6652ac
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-12-06 10:48:41 +0200

    added client

--- src/health-check-service/main.rs
+++ src/health-check-service/main.rs
@@ -18,7 +18,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // ::0 is required for Docker to work: https://stackoverflow.com/questions/59179831/docker-app-server-ip-address-127-0-0-1-difference-of-0-0-0-0-ip
     let auth_hostname = env::var("AUTH_SERVICE_HOST_NAME").unwrap_or("[::0]".to_owned());
 
-    // Establish connection when auth service
     let mut client = AuthClient::connect(format!("http://{}:50051", auth_hostname)).await?;
 
     loop {
